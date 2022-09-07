c98a303a944e01af4612818b34435f25fdf68aaf
olivibernard <obernard79@gmail.com>
2022-09-07 09:56:08 +0200

    .

--- classes/day7.py
+++ classes/day7.py
@@ -1,4 +1,12 @@
-class Dog():
+class Animal():
+    noise = 'grunt'
+    size = 'Large'
+
+    def get_size(self):
+        return self.size
+
+
+class Dog(Animal):
     name = 'a'
     color = 'brown'
 
@@ -8,6 +16,14 @@ class Dog():
     def get_color(self):
         return self.color
 
+    def set_color(self, this_color):
+        self.color = this_color
+    
+    def get_noise(self):
+        return self.noise
 obj = Dog()
-print(obj.get_color())
-print(obj.get_name())
+print(obj.get_name() + ' is ' + obj.get_color())
+print(obj.set_color('blue'))
+print(obj.get_name() + ' is ' + obj.get_color())
+print(obj.get_name() + ' make this noise : ' + obj.get_noise())
+print(obj.get_name() + ' is ' + obj.get_size())
